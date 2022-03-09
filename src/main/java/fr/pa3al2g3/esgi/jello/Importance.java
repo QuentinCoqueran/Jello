7360129944a0ef3a75fefbf7b549f495961f60d7
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-03-09 14:17:43 +0100

    feature/add Importance enum

---
+++ src/main/java/fr/pa3al2g3/esgi/jello/Importance.java
@@ -0,0 +1,18 @@
+package fr.pa3al2g3.esgi.jello;
+
+public enum Importance {
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High");
+
+    private final String title;
+
+    private Importance(String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
