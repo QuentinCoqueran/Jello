704888224357b6e98e5a41824336722aba5db8b4
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-03-25 22:41:28 +0100

    feature: Add Home-view

--- src/main/java/fr/pa3al2g3/esgi/jello/MainApplication.java
+++ src/main/java/fr/pa3al2g3/esgi/jello/MainApplication.java
@@ -7,15 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     private static Stage stage;
     private static Rectangle2D screenBounds;
+    private static Window window;
     @Override
     public void start(Stage stage) throws IOException {
-
         MainApplication.stage = stage;
         Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
         Scene scene = new Scene(root);
@@ -23,16 +24,22 @@ public class MainApplication extends Application {
         stage.setMaximized(true);
         MainApplication.screenBounds = Screen.getPrimary().getBounds();
         stage.show();
+        MainApplication.window = stage.getScene().getWindow();
     }
 
     public static void setScene(Scene scene){
         stage.setScene(scene);
         stage.setHeight(screenBounds.getHeight());
         stage.setWidth(screenBounds.getWidth());
+        MainApplication.window = stage.getScene().getWindow();
         stage.setMaximized(true);
     }
     public static Scene getScene(){
         return stage.getScene();
+    }
+
+    public static Window getWindow() {
+        return window;
     }
 
     public static void main(String[] args) {launch();}
--- src/main/java/fr/pa3al2g3/esgi/jello/model/HomeModel.java
+++ src/main/java/fr/pa3al2g3/esgi/jello/model/HomeModel.java
@@ -8,14 +8,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
 
 public class HomeModel {
     private ArrayList<String> projects;
 
-    public HomeModel(){
+    public HomeModel() {
         this.projects = new ArrayList<>();
         addProject("Test");
         addProject("aBc");
@@ -23,49 +26,85 @@ public class HomeModel {
         addProject("aBc");
         addProject("Test");
         addProject("aBc");
+        addProject("Test");
+        addProject("aBc");
+        addProject("Test");
+        addProject("aBc");
+        addProject("Test");
+        addProject("aBc");
+        addProject("Test");
+
+
+
+
 
         init();
     }
 
-    private void addProject(String projectName){
+    private void addProject(String projectName) {
         this.projects.add(projectName);
     }
 
-    private void init(){
+    private void init() {
         ScrollPane scrollPane = (ScrollPane) MainApplication.getScene().lookup("#project_scroll_pane");
-        GridPane grid = new GridPane();
-        grid.setStyle("-fx-background-color: yellow");
-        //grid.setAlignment(Pos.CENTER);
-        if(projects.size() == 1){
-            grid.setPrefHeight(74);
-            grid.setPrefWidth(486);
+        scrollPane.setStyle("-fx-background-color: #FFE4B5");
+        scrollPane.setStyle("-fx-background: #FFE4B5");
+        scrollPane.getStyleClass().add("edge-to-edge");
+
+        GridPane scrollGrid = new GridPane();
+        int row = 0;
+        for (String s : projects) {
+
+            GridPane gridButton = new GridPane();
+
+            Pane emptySpaceButtonLeft = new Pane();
+            emptySpaceButtonLeft.setPrefHeight(this.calcHeigth(74));
+            emptySpaceButtonLeft.setPrefWidth(this.calcWidth(55));
+
+            Pane emptySpaceButtonRight = new Pane();
+            emptySpaceButtonRight.setPrefHeight(this.calcHeigth(74));
+            emptySpaceButtonRight.setPrefWidth(this.calcWidth(55));
+
             Button btn = new Button();
-            btn.setText(projects.get(0));
-            grid.add(btn, 0, 0);
-        }else{
-            int calcSize = (74 * projects.size()) + (64 * (projects.size() - 1));
-            grid.setPrefHeight(calcSize);
-            grid.setPrefWidth(540);
-            int row = 0;
-            for(String s : projects){
-                Button btn = new Button();
-                btn.setText(s);
-                btn.setPrefHeight(74);
-                btn.setPrefWidth(400);
-                if(row != projects.size() || row != 0){
-                    grid.addRow(row, btn);
-                    row = row + 1;
-                    Text emptyText = new Text("");
-                    emptyText.prefHeight(64);
-                    grid.add(emptyText, 0, row);
-                    row = row + 1;
-                }else {
-                    grid.add(btn, 0, row);
-                    row = row + 1;
-                }
-                GridPane.setHalignment(btn, HPos.CENTER);
+            btn.setText(s);
+            btn.setFont(new Font(20));
+            btn.setStyle("-fx-font-weight: 800");
+            btn.setTextAlignment(TextAlignment.CENTER);
+            btn.setTextFill(Paint.valueOf("white"));
+            btn.setStyle("-fx-background-color: #FF8248");
+            btn.setPrefHeight(this.calcHeigth(74));
+            btn.setPrefWidth(this.calcWidth(372));
+
+            if(projects.size() >= 9){
+                emptySpaceButtonLeft.setPrefWidth(this.calcWidth(53));
+                emptySpaceButtonRight.setPrefWidth(this.calcWidth(53));
+                btn.setPrefWidth(this.calcWidth(367));
             }
+
+            gridButton.add(emptySpaceButtonLeft, 0, 0);
+            gridButton.add(btn, 1, 0);
+            gridButton.add(emptySpaceButtonRight, 2, 0);
+
+            scrollGrid.addRow(row, gridButton);
+            row = row + 1;
+
+            Pane emptySpace = new Pane();
+            emptySpace.setPrefHeight(this.calcHeigth(66));
+
+            scrollGrid.add(emptySpace, 0, row);
+            row = row + 1;
+
         }
-        scrollPane.setContent(grid);
+        scrollPane.setContent(scrollGrid);
+
+
+    }
+
+    private double calcWidth(double size) {
+        return ((size * 100 / 1728) * (MainApplication.getWindow().getWidth() / 100));
+    }
+
+    private double calcHeigth(double size) {
+        return ((size * 100 / 1728) * (MainApplication.getWindow().getHeight() / 100));
     }
 }
