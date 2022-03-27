2683cf1db28fa1ca34d480c5f46ab5b855f438e5
David <zdavid1@myges.fr>
2022-03-27 20:18:27 +0200

    manager

--- src/main/java/fr/pa3al2g3/esgi/jello/manager/ModelManager.java
+++ src/main/java/fr/pa3al2g3/esgi/jello/manager/ModelManager.java
@@ -2,15 +2,18 @@ package fr.pa3al2g3.esgi.jello.manager;
 
 import fr.pa3al2g3.esgi.jello.model.HomeModel;
 import fr.pa3al2g3.esgi.jello.model.ProjectModel;
+import fr.pa3al2g3.esgi.jello.model.TableModel;
 
 public class ModelManager {
 
     private HomeModel homeModel;
     private ProjectModel projectModel;
+    private TableModel tableModel;
 
-    public ModelManager(){
+    public ModelManager() {
         this.homeModel = new HomeModel();
         this.projectModel = new ProjectModel();
+        this.tableModel = new TableModel();
     }
 
     public HomeModel getHomeModel() {
@@ -19,5 +22,9 @@ public class ModelManager {
 
     public ProjectModel getProjectModel() {
         return projectModel;
+    }
+
+    public TableModel getTableModel() {
+        return tableModel;
     }
 }
