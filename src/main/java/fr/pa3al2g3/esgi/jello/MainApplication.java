f07a0d21eec7258594caad1acd9a589a4c55883d
Mathieu <ferreira.mathieu.pro@gmail.com>
2022-04-08 23:43:46 +0200

    fix

--- src/main/java/fr/pa3al2g3/esgi/jello/MainApplication.java
+++ src/main/java/fr/pa3al2g3/esgi/jello/MainApplication.java
@@ -31,14 +31,13 @@ public class MainApplication extends Application {
         MainApplication.instance = getClass();
         MainApplication.stage = stage;
         MainApplication.modelManager = new ModelManager();
-        Parent root = FXMLLoader.load(getClass().getResource("project-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("connection-view.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         //stage.setMaximized(true);
         MainApplication.screenBounds = Screen.getPrimary().getBounds();
         stage.show();
         MainApplication.window = stage.getScene().getWindow();
-        MainApplication.getModelManager().getProjectModel().init(1);
     }
 
     public static Connection getDatabase(){
